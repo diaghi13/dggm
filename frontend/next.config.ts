ace6f501f23e358be73b67f059bd552fbd05ee48
Davide Donghi <davide.d.donghi@gmail.com>
2026-01-14 21:48:04 +0100

    Next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'standalone',
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://dggm-erp.ddns.net/api/v1',
+    },
 };
 
 export default nextConfig;
