6d3f9a730c8a6c5ee38a4f9e0a2d0987798d555d
Davide Donghi <davide.d.donghi@gmail.com>
2026-03-31 11:43:22 +0200

    Add landlord seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Landlord\GlobalUser;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,52 +16,61 @@ class DatabaseSeeder extends Seeder
     {
         // Seed roles and permissions first
         $this->call([
-            RoleAndPermissionSeeder::class,
-            DiscountFamilySeeder::class,
-            PaymentTermSeeder::class,
-            ProductCategorySeeder::class,
-            ProductRelationTypeSeeder::class,
-            SettingSeeder::class,
-            AppSettingsSeeder::class,
-            CompanySettingsSeeder::class,
-            QuoteSettingsSeeder::class,
-            ProjectRoleSeeder::class,
-            WarrantyTypeSeeder::class,
-            RentalProfileSeeder::class,
+//            RoleAndPermissionSeeder::class,
+//            DiscountFamilySeeder::class,
+//            PaymentTermSeeder::class,
+//            ProductCategorySeeder::class,
+//            ProductRelationTypeSeeder::class,
+//            SettingSeeder::class,
+//            AppSettingsSeeder::class,
+//            CompanySettingsSeeder::class,
+//            QuoteSettingsSeeder::class,
+//            ProjectRoleSeeder::class,
+//            WarrantyTypeSeeder::class,
+//            RentalProfileSeeder::class,
         ]);
 
-        // Create a super admin user
-        $superAdmin = User::firstOrCreate(
-            ['email' => 'admin@dggm.com'],
+        $superAdmin = GlobalUser::firstOrCreate(
+            ['email' => 'davide.d.donghi@gmail.com'],
             [
-                'name' => 'Super Admin',
-                'password' => Hash::make('password'),
+                'name' => 'Davide Donghi',
+                'password' => Hash::make('Imbues0868$'),
+                'is_landlord_admin' => true,
             ]
         );
-        $superAdmin->assignRole('super-admin');
+
+        // Create a super admin user
+//        $superAdmin = User::firstOrCreate(
+//            ['email' => 'admin@dggm.com'],
+//            [
+//                'name' => 'Super Admin',
+//                'password' => Hash::make('password'),
+//            ]
+//        );
+//        $superAdmin->assignRole('super-admin');
 
         // Create test users for each role
-        $projectManager = User::firstOrCreate(
-            ['email' => 'pm@dggm.com'],
-            [
-                'name' => 'Project Manager',
-                'password' => Hash::make('password'),
-            ]
-        );
-        $projectManager->assignRole('project-manager');
+//        $projectManager = User::firstOrCreate(
+//            ['email' => 'pm@dggm.com'],
+//            [
+//                'name' => 'Project Manager',
+//                'password' => Hash::make('password'),
+//            ]
+//        );
+//        $projectManager->assignRole('project-manager');
 
-        $worker = User::firstOrCreate(
-            ['email' => 'worker@dggm.com'],
-            [
-                'name' => 'Worker',
-                'password' => Hash::make('password'),
-            ]
-        );
-        $worker->assignRole('worker');
+//        $worker = User::firstOrCreate(
+//            ['email' => 'worker@dggm.com'],
+//            [
+//                'name' => 'Worker',
+//                'password' => Hash::make('password'),
+//            ]
+//        );
+//        $worker->assignRole('worker');
 
-        $this->command?->info('Database seeded successfully!');
-        $this->command?->info('Super Admin: admin@dggm.com / password');
-        $this->command?->info('Project Manager: pm@dggm.com / password');
-        $this->command?->info('Worker: worker@dggm.com / password');
+//        $this->command?->info('Database seeded successfully!');
+//        $this->command?->info('Super Admin: admin@dggm.com / password');
+//        $this->command?->info('Project Manager: pm@dggm.com / password');
+//        $this->command?->info('Worker: worker@dggm.com / password');
     }
 }
